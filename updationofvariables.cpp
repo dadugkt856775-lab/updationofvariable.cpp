166bd13702977454beb02aed7bf3d845cc3407f3
Dadakhalandar S A <dadugkt856775@gmail.com>
2026-05-25 00:43:09 +0530

    Add files via upload

---
+++ updationofvariables.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x = 6;
+    cout<<x<<endl;
+    x = x + 1114;   // so x valu is 1120
+    cout<<x<<endl;
+    x = x - 1051;    // here x valu is 69
+    cout<<x<<endl;
+}
